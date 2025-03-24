0a1f4a4dab671e085dad39e73f377e129825708f
LiaCBA <moonlightt1083@gmail.com>
2025-03-24 13:14:43 +0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Person from "./component/Person";
+import Product from "./component/Product";
+
+function App() {
+  return (
+    <div>
+      <Person name="Lia" age="17" />
+      <Product name="Apple" price="1$" />
+    </div>
+  );
+}
+
+export default App;
